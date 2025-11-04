dc7175138fbf9dff2dc831157933959ce5c2122e
Homayoun <homayoun763@gmail.com>
2025-11-04 22:53:19 +0330

    add : project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className="text-2xl text-center m-5 font-bold">
+      Vite React starter with TypeScript & Tailwind CSS
+    </h1>
+  );
+};
+
+export default App;
