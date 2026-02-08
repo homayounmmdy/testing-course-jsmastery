564b676f8e8b4f24723f329319bc258d5fdc6772
Homayoun <homayoun763@gmail.com>
2026-02-08 22:22:34 +0330

    Mock Question Card Components

--- devflow/tests/mocks/index.ts
+++ devflow/tests/mocks/index.ts
@@ -2,6 +2,10 @@ export * from "./router.mock";
 export * from "./toast.mock";
 export * from "./editor.mock";
 export * from "./nextauth.mock";
+export * from "./editdeleteaction.mock";
+export * from "./image.mock";
+export * from "./link.mock";
+export * from "./metric.mock";
 
 export const resetAllMocks = () => {
   jest.clearAllMocks();
