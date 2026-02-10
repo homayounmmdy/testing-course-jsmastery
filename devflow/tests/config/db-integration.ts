a5a48fb599a3a4327f1a5b7e33c3420ab823c8b3
Homayoun <homayoun763@gmail.com>
2026-02-10 20:10:14 +0330

    Database Setup

---
+++ devflow/tests/config/db-integration.ts
@@ -0,0 +1,64 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongoServer: MongoMemoryServer;
+let isConnected = false;
+
+export async function connectToDB(): Promise<void> {
+  if (isConnected) return;
+  mongoServer = await MongoMemoryServer.create();
+  const uri = MongoMemoryServer.getUri();
+
+  await mongoose.connect(uri, { dbName: "testingDB" });
+
+  if (global.mongoose) {
+    global.mongoose.conn = mongoose;
+    global.mongoose.promise = Promise.resolve(mongoose);
+  }
+
+  isConnected = true;
+  try {
+  } catch (e) {
+    console.error("Failed to connect to integration DB: ", e);
+    throw e;
+  }
+}
+
+export async function disconnectToDB(): Promise<void> {
+  if (!isConnected) return;
+
+  try {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+
+    if (mongoServer) {
+      await mongoServer.stop();
+    }
+
+    if (global.mongoose) {
+      global.mongoose.conn = null;
+      global.mongoose.promise = null;
+    }
+
+    isConnected = false;
+  } catch (e) {
+    console.error("Failed to disconnect from integration DB: ", e);
+    throw e;
+  }
+}
+
+export async function clearDB(): Promise<void> {
+  if (!isConnected) {
+    throw new Error("Database not  connected");
+  }
+  try {
+    await mongoose.connection.dropDatabase();
+  } catch (e) {
+    console.error("Failed to clear the integration DB: ", e);
+    throw e;
+  }
+}
+
+export function isDBConnected(): boolean {
+  return isConnected && mongoose.connection.readyState === 1;
+}
